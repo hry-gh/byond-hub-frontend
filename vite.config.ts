0a088b73870eaf4ccb5f309f8f44e6c3873ed76d
harryob <55142896+harryob@users.noreply.github.com>
2026-02-26 16:24:23 +0000

    basepath NOT required

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  base: process.env.BASE_PATH || "/",
   plugins: [
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
